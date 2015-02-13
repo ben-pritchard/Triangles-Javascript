0c4f5893e33566cda9da1c8f24ef6fe59fc94bf3
Ben <ben.s.pritchard@gmail.com>
2015-02-13 09:57:36 -0800

    Add specs and logic to return the type of triangle based on given sides.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ var triangle = function(side1, side2, side3) {
   sides.sort();
 
   if (sides[0] + sides[1] <= sides[2]) {
-    return "The side lengths you gave can't form a triangle!"
+    return "The side lengths you gave can't form a triangle!";
   };
+
+  if ((side1 === side2) && (side1 === side3)) {
+    return "equilateral";
+  };
+
+  if ((side1 === side2) || (side1 === side3) || (side2 === side3)) {
+    return "isosceles";
+  };
+
+  return "scalene";
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,5 +3,15 @@ describe("triangle", function(side1, side2, side3) {
     expect(triangle(3, 5, 1)).to.equal("The side lengths you gave can't form a triangle!");
   });
 
+  it("returns 'equilateral' if all three sides are of equal length", function() {
+    expect(triangle(3, 3, 3)).to.equal("equilateral");
+  });
 
+  it("returns 'isosceles' if two of the sides are of equal length", function() {
+    expect(triangle(9, 9, 7)).to.equal("isosceles");
+  });
+
+  it("returns 'scalene' if none of the sides are of equal length", function() {
+    expect(triangle(9, 4, 7)).to.equal("scalene");
+  });
 });
