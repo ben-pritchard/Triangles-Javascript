b68d45a228f9b84b2e60dd4847a38090a84f0b3d
Ben <ben.s.pritchard@gmail.com>
2015-02-13 10:11:10 -0800

    Add jquery and triangles.html code for website functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,19 @@ var triangle = function(side1, side2, side3) {
 
   return "scalene";
 }
+
+
+$(document).ready(function() {
+  $("form#sides").submit(function(event) {
+
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    var result = triangle(side1, side2, side3)
+
+    $("#result").text(result);
+
+    event.preventDefault();
+  });
+});
